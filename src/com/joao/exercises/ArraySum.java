434eac1370db0125754ede09270449fa588cc2ca
Joaopmaia-DEV <csgofreemarquin2@gmail.com>
2026-03-19 12:48:00 -0300

    feat: add ArraySum exercise (sum of array elements)

--- src/com/joao/exercises/ArraySum.java
+++ src/com/joao/exercises/ArraySum.java
@@ -1,4 +1,14 @@
 package com.joao.exercises;
 
 public class ArraySum {
+    static void main(String[] args) {
+        int[] arr = new int[]{1,2,3,4,5,6,7,8};
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        double media = (double) sum / arr.length;
+        System.out.println(sum);
+        System.out.println(media);
+    }
 }
