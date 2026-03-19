c9141fe6bba229bfb4cfce474d73ffe646a11a73
Joaopmaia-DEV <csgofreemarquin2@gmail.com>
2026-03-19 12:58:07 -0300

    feat: add GreaterThanTen exercise (filter values > 10)

---
+++ src/com/joao/exercises/GreaterThanTen.java
@@ -0,0 +1,12 @@
+package com.joao.exercises;
+
+public class GreaterThanTen {
+     public void main(String[] args) {
+        int[] arr = new int[]{5, 12, 3, 20, 8, 15};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 10){
+                System.out.println(arr[i]);
+            }
+        }
+    }
+}
