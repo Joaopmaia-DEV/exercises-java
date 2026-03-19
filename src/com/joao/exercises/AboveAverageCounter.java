8f90f69dcef6f87a94946514046563c52c279df1
Joaopmaia-DEV <csgofreemarquin2@gmail.com>
2026-03-19 15:14:11 -0300

    feat: add AboveAverageCounter exercise (calculate average and count values above it)

---
+++ src/com/joao/exercises/AboveAverageCounter.java
@@ -0,0 +1,22 @@
+package com.joao.exercises;
+
+public class AboveAverageCounter {
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 6, 8};
+        double average;
+        int aboveAverage = 0;
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        average = (double) sum / arr.length;
+        for (int num : arr) {
+            if (num > average) {
+                aboveAverage++;
+            }
+        }
+        System.out.println("Average: " + average);
+        System.out.println("Above average: " + aboveAverage);
+
+    }
+}
