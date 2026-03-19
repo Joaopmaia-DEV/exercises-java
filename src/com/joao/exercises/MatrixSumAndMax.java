97eb21f8656626470564ac530f366574ef3270b5
Joaopmaia-DEV <csgofreemarquin2@gmail.com>
2026-03-19 15:52:58 -0300

    feat: add MatrixSumAndMax exercise (calculate sum and max value of a matrix)

---
+++ src/com/joao/exercises/MatrixSumAndMax.java
@@ -0,0 +1,21 @@
+package com.joao.exercises;
+
+public class MatrixSumAndMax {
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int maxValue = matrix[0][0];
+        int sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] > maxValue) {
+                    maxValue = matrix[i][j];
+                }
+                sum += matrix[i][j];
+            }
+        }
+        System.out.println("Sum: " + sum);
+        System.out.println("Max: " + maxValue);
+
+    }
+
+}
