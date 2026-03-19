cdc32a03164b8d6f773c2fb96a47903db6fa3b5d
Joaopmaia-DEV <csgofreemarquin2@gmail.com>
2026-03-19 14:25:23 -0300

    feat: add EvenOddCounter exercise (count even and odd numbers)

---
+++ src/com/joao/exercises/EvenOddCounter.java
@@ -0,0 +1,18 @@
+package com.joao.exercises;
+
+public class EvenOddCounter {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int evenCount = 0;
+        int oddCount = 0;
+        for (int num : arr) {
+            if (num % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+        System.out.println("Even: " + evenCount);
+        System.out.println("Odd: " + oddCount);
+    }
+}
