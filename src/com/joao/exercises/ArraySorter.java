dc3c966633990da1e117e30e5d2acc881c24e65d
Joaopmaia-DEV <csgofreemarquin2@gmail.com>
2026-03-19 23:11:41 -0300

    feat: add ArraySorter exercise using custom sorting logic

---
+++ src/com/joao/exercises/ArraySorter.java
@@ -0,0 +1,23 @@
+package com.joao.exercises;
+
+public class ArraySorter {
+    public static void main(String[] args) {
+        int memory;
+        int[] arr = {5, 2, 8, 1, 3};
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = arr.length; j > 1; j--) {
+                if (i >= arr.length - j + 1)
+                    continue;
+                if (arr[i] > arr[arr.length - j + 1]) {             // Custom sorting logic; Moves smaller elements to the beginning of the array
+                    memory = arr[arr.length - j + 1];
+                    arr[arr.length - j + 1] = arr[i];
+                    arr[i] = memory;
+                }
+            }
+        }
+        for (int i : arr) {
+            System.out.println(i);
+        }
+
+    }
+}
